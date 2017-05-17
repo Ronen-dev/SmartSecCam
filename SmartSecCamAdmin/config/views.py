70ba50f506998e854defa5caa89a6ca5d0c1d679
mazron <ronenn.ma@gmail.com>
2017-05-17 22:06:36 +0200

    Modification changement de mot de passe

--- SmartSecCamAdmin/config/views.py
+++ SmartSecCamAdmin/config/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import os
 
 
 def index(request):
-    context = {
-        'test': request,
-    }
+    if request.method == 'POST':
+        context = {
+            'test': request.POST.get('password', '')
+        }
+
+        filename = '.pwd'
+
+        if os.path.getsize('.pwd') > 0:
+            context['error'] = "Un mot de passe est déjà défini."
+        else:
+            file = open(filename, "w")
+            file.write(request.POST.get('password', ''))
+            file.close()
+
+            context['success'] = "Mot de passe correctement modifié: " + request.POST.get('password', '')
+
+            # return render(request, 'config/wifi.html', context)
+            return redirect('wifi/', )
+    else:
+        context = {
+            'test': "GET OK"
+        }
     return render(request, 'config/index.html', context)
 
 
