c8ce561f680b77bbac2466c3de0e0ac7e3b117fe
mazron <ronenn.ma@gmail.com>
2017-05-18 10:33:01 +0200

    Avancement mot de passe

--- SmartSecCamAdmin/config/views.py
+++ SmartSecCamAdmin/config/views.py
@@ -1,31 +1,34 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 import os
 
 
 def index(request):
-    if request.method == 'POST':
-        context = {
-            'test': request.POST.get('password', '')
-        }
+    context = {}
 
+    # POST case
+    if request.method == 'POST':
+
+        password = request.POST.get('password', '')
         filename = '.pwd'
 
+        # Si le mot de passe est déjà défini
         if os.path.getsize('.pwd') > 0:
             context['error'] = "Un mot de passe est déjà défini."
+
+            return render()
+        # Sinon
         else:
             file = open(filename, "w")
-            file.write(request.POST.get('password', ''))
+            file.write(password)
             file.close()
 
-            context['success'] = "Mot de passe correctement modifié: " + request.POST.get('password', '')
+            context['success'] = "Mot de passe correctement modifié : " + password
 
-            # return render(request, 'config/wifi.html', context)
-            return redirect('wifi/', )
+            return render(request, 'config/wifi.html', context)
+
+    # GET case
     else:
-        context = {
-            'test': "GET OK"
-        }
-    return render(request, 'config/index.html', context)
+        return render(request, 'config/index.html', context)
 
 
 def wifi(request):
