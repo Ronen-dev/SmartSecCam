fbc83aa399b0aab3f44d19326189ce3602b1845b
badr-dev <badredine.belhadef@epitech.eu>
2017-05-18 17:47:35 +0200

    test Scan wifi @bad

--- SmartSecCamAdmin/config/views.py
+++ SmartSecCamAdmin/config/views.py
@@ -1,4 +1,106 @@
+import wifi
+from wifi import Cell, Scheme
 from django.shortcuts import render
+
+
+
+
+# method not for view
+def Search():
+    wifilist = []
+
+    cells = Cell.all('wlan0')
+
+    for cell in cells:
+        wifilist.append(cell)
+
+    return wifilist
+
+
+def FindFromSearchList(ssid):
+    wifilist = Search()
+
+    for cell in wifilist:
+        if cell.ssid == ssid:
+            return cell
+
+    return False
+
+
+def FindFromSavedList(ssid):
+    cell = wifi.Scheme.find('wlan0', ssid)
+
+    if cell:
+        return cell
+
+    return False
+
+
+def Connect(ssid, password=None):
+    cell = FindFromSearchList(ssid)
+
+    if cell:
+        savedcell = FindFromSavedList(cell.ssid)
+
+        # Already Saved from Setting
+        if savedcell:
+            savedcell.activate()
+            return cell
+
+        # First time to conenct
+        else:
+            if cell.encrypted:
+                if password:
+                    scheme = Add(cell, password)
+
+                    try:
+                        scheme.activate()
+
+                    # Wrong Password
+                    except wifi.exceptions.ConnectionError:
+                        Delete(ssid)
+                        return False
+
+                    return cell
+                else:
+                    return False
+            else:
+                scheme = Add(cell)
+
+                try:
+                    scheme.activate()
+                except wifi.exceptions.ConnectionError:
+                    Delete(ssid)
+                    return False
+
+                return cell
+
+    return False
+
+
+def Add(cell, password=None):
+    if not cell:
+        return False
+
+    scheme = wifi.Scheme.for_cell('wlan0', cell.ssid, cell, password)
+    scheme.save()
+    return scheme
+
+
+def Delete(ssid):
+    if not ssid:
+        return False
+
+    cell = FindFromSavedList(ssid)
+
+    if cell:
+        cell.delete()
+        return True
+
+    return False
+
+#end method
+
 
 
 def index(request):
@@ -9,5 +111,14 @@ def index(request):
 
 
 def wifi(request):
-    context = {}
-    return render(request, 'config/wifi.html', context)
+
+    if request.method == "GET":
+
+        context = { 'wifilist': Search() }
+
+        return render(request, 'config/wifi.html', context)
+
+    else:
+
+        context = { 'list' : request.POST.get('wifi_name', "")}
+        return render(request, 'config/wifi.html', context)
