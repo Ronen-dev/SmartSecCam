ec1403bc9a5c01bbaf8b285fc7059ef33fa3d7c6
Adrien Nadal <nadal_a@etna-alternance.net>
2017-06-01 22:54:33 +0200

    modification du repertoire

---
+++ Trash/camera/testStreamimg/refonte/server.py
@@ -0,0 +1,72 @@
+import io
+import socket
+import struct
+from PIL import Image
+import cv2
+import numpy
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+
+
+
+
+#fonction de traitement de la frame
+def extract_features(image):
+    print('Extract features')
+    face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.0.0/data/haarcascades/haarcascade_frontalface_default.xml')
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # iterate over all identified faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = image[y:y+h, x:x+w]
+
+    cv2.imshow('SmartSecCam', image)
+    print('after show')
+
+# Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
+# all interfaces)
+server_socket = socket.socket()
+server_socket.bind(('localhost', 8000))
+server_socket.listen(0)
+
+# Accept a single connection and make a file-like object out of it
+connection = server_socket.accept()[0].makefile('rb')
+try:
+    while True:
+        # Read the length of the image as a 32-bit unsigned int. If the
+        # length is zero, quit the loop
+        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        if not image_len:
+            break
+        # Construct a stream to hold the image data and read the image
+        # data from the connection
+        image_stream = io.BytesIO()
+        image_stream.write(connection.read(image_len))
+        # Rewind the stream, open it as an image with PIL and do some
+        # processing on it
+        image_stream.seek(0)
+        image = Image.open(image_stream)
+        print('Image is %dx%d' % image.size)
+        opencvImage = cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2BGR)        
+        key = cv2.waitKey(1) & 0xFF
+        extract_features(opencvImage)
+        if key == ord("q"):
+            sock.close()
+            break
+        #image.verify()
+        print('Image is verified')
+finally:
+    connection.close()
+    server_socket.close()
---
+++ webservice/webservice.py
@@ -0,0 +1,183 @@
+import os, random, string, base64
+from flask import Flask, render_template, url_for, request, redirect
+from simplecrypt import encrypt, decrypt
+from wifi import Cell, Scheme
+from urllib.request import urlopen
+from urllib.error import URLError
+
+app = Flask(__name__)
+wifiInterface = 'wlan0'
+
+connected = False
+
+def checkConnection():
+    global connected
+    try:
+        urlopen('http://www.google.fr', timeout=1)
+        connected = True
+    except URLError:
+        connected = False
+
+def tryToConnect():
+    global connected
+    file = open('.known_wifi', 'r')
+    for line in file:
+        scheme = Scheme.find(wifiInterface, line)
+        if scheme:
+            try:
+                scheme.activate()
+            except ConnectionError:
+                continue
+            connected = True
+    connected = False
+
+checkConnection()
+
+if not connected:
+    tryToConnect()
+
+
+@app.route('/')
+def index():
+    checkSecretFile()
+    checkKnownWifiFile()
+    if not checkPasswordFile():
+        return redirect(url_for('password'))
+    if not connected:
+        return redirect(url_for('wifi'))
+    if connected:
+        return redirect(url_for('ip'))
+    return 'Welcome on SmartSecCam web interface'
+
+@app.route('/password', methods=['GET', 'POST'])
+def password():
+    error = None
+    if checkPasswordFile():
+        redirect(url_for('index'))
+    if request.method == 'GET':
+        return render_template('password.html', error=error)
+    if request.method == 'POST':
+        if request.form['password'] and request.form['confirm']:
+            if request.form['password'] != request.form['confirm']:
+                error = "les deux mots de passes doivent être identiques"
+                return render_template('password.html', error=error)
+            # changer le mdp root de la machine
+            file = open('.password', 'wb')
+            password = appEncrypt(request.form['password'])
+            file.write(password)
+            file.close()
+
+            ####
+            #                  MODIFICATION DU MOT DE PASSE
+            #   COMMENTER CETTE PARTIE POUR NE PAS AVOIR UNE MAUVAISE SURPRISE !
+            ####
+
+            # sudo_password_callback = 'azerty'
+            # username, username_password = 'pi', password
+            #
+            # try:
+            #     hashed = crypt(username_password)
+            # except TypeError:
+            #     p = Popen(
+            #         ["mkpasswd", "-m", "sha-512", "-s"],
+            #         stdin=PIPE,
+            #         stdout=PIPE,
+            #         universal_newlines=True
+            #     )
+            #
+            #     hashed = p.communicate(username_password)[0][:-1]
+            #     assert p.wait() == 0
+            # assert hashed == crypt(username_password, hashed)
+            #
+            # p = Popen(
+            #     ['sudo', '-S', 'usermod', '-p', hashed, username],
+            #     stdin=PIPE,
+            #     universal_newlines=True
+            # )
+            #
+            # p.communicate(sudo_password_callback + '\n')
+            # assert p.wait() == 0
+
+            #
+            # FIN MODIFICATION MOT DE PASSE
+            #
+
+            return redirect(url_for('index'))
+        else:
+            error = "Vous devez remplir les deux champs ci-dessous"
+            return render_template('password.html', error=error)
+
+@app.route('/wifi', methods=['GET', 'POST'])
+def wifi():
+    error = None
+    if request.method == 'GET':
+        return render_template('wifi.html', error=error)
+    if request.method == 'POST':
+        if request.form['wifi-name'] and request.form['wifi-key']:
+            try:
+                cell = searchCell(request.form['wifi-name'])
+                if cell:
+                    scheme = Scheme.for_cell(wifiInterface, request.form['wifi-name'], cell, request.form['wifi-key'])
+                    scheme.save()
+                    scheme.activate()
+                else:
+                    error = 'Le wifi n\'a pas été trouvé veuillez en renseigner un autre'
+                    return render_template('wifi.html', error=error)
+            except ConnectionError:
+                error = 'Impossible de se connecter au réseau veuillez renseigner des informations valides'
+                return render_template('wifi.html', error=error)
+            file = open('.known_wifi', 'a')
+            file.write(request.form['wifi-name'] + '\n')
+            file.close()
+            return redirect(url_for('ip'))
+        else:
+            error = 'Vous devez remplir les deux champs du formulaire'
+            return render_template('wifi.html', error=error)
+
+@app.route('/ip')
+def ip():
+    ip = urlopen('http://ip.42.pl/raw').read().decode('utf-8')
+    return render_template('ip.html', ip=ip)
+
+
+def checkSecretFile():
+    directory = os.path.dirname(os.path.realpath(__file__))
+    if not os.path.isfile(directory + '/.secret'):
+        file = open('.secret', 'w')
+        chars = string.ascii_letters + string.digits + string.punctuation
+        result = ''.join(random.choice(chars) for _ in range(64))
+        file.write(result)
+        file.close()
+
+def checkPasswordFile():
+    directory = os.path.dirname(os.path.realpath(__file__))
+    if not os.path.isfile(directory + '/.password'):
+        return False
+    return True
+
+def checkKnownWifiFile():
+    directory = os.path.dirname(os.path.realpath(__file__))
+    if not os.path.isfile(directory + '/.known_wifi'):
+        file = open('.known_wifi', "w")
+        file.close()
+
+def appEncrypt(word):
+    file = open('.secret', 'r')
+    secret_key = file.readline()
+    cipher = encrypt(secret_key, word)
+    file.close()
+    return cipher
+
+def appDecrypt(word):
+    file = open('.secret', 'r')
+    secret_key = file.readline()
+    msg = decrypt(secret_key, word).decode('utf-8')
+    file.close()
+    return (msg)
+
+def searchCell(name):
+    cells = Cell.all(wifiInterface)
+    for cell in cells:
+        if cell.ssid == name:
+            return cell
+    return None
